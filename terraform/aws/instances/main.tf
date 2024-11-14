45011f11e2935081180bb9c84c0ab40e14922ec6
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-11-14 13:46:39 -0700

    Added terraform instances main.tf

---
+++ terraform/aws/instances/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ec2_instance" {
+  source = "../modules/ec2"
+
+  instance_name  = "k8s-node"
+  ami_id         = "ami-0866a3c8686eaeeba"
+  instance_type  = "t2.medium"
+  key_name       = "devops1"
+  subnet_ids     = ["subnet-0caef41d3608c34ef", "subnet-09d2f4fbd2bffd05a"]
+  instance_count = 3
+
+  inbound_from_port  = ["0", "6443", "22", "30000"]
+  inbound_to_port    = ["65000", "6443", "22", "32768"]
+  inbound_protocol   = ["TCP", "TCP", "TCP", "TCP"]
+  inbound_cidr       = ["172.31.0.0/16", "0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
+  outbound_from_port = ["0"]
+  outbound_to_port   = ["0"]
+  outbound_protocol  = ["-1"]
+  outbound_cidr      = ["0.0.0.0/0"]
+}
