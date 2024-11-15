515f1e4e6b661b4380f83a3e30033e719b9610d3
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-11-14 20:10:08 -0700

    Added metrics-server.yaml

--- ansible/roles/kubeadm/files/common.sh
+++ ansible/roles/kubeadm/files/common.sh
@@ -32,7 +32,7 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-# Apply sysctl params without reboot
+# Apply sysctl params without reboots
 sudo sysctl --system
 
 sudo apt-get update -y
