a8b5e6e5513bc940e78ca3db61f3482bf84c23c5
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2024-11-19 00:56:30 -0700

    Updated

--- terraform/aws/instances/backend.tf
+++ terraform/aws/instances/backend.tf
@@ -3,7 +3,6 @@ terraform {
      bucket         = "gcp-new-microservices"
      key            = "global/s3/terraform.tfstate"
      region         = "us-east-1"
-     dynamodb_table = "gcp-remote-locking"
      encrypt        = true
    }
 }
